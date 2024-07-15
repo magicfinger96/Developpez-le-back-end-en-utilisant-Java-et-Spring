a1b635541a73f2782c4a72f306aa044b2522a9c8
Andrea Franco <dedefac96@gmail.com>
2024-07-15 10:15:13 +0200

    Created the custom user details service

---
+++ Back/src/main/java/com/openclassrooms/RentalProject/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.openclassrooms.RentalProject.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.openclassrooms.RentalProject.model.User;
+import com.openclassrooms.RentalProject.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository dbUserRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = dbUserRepository.findByEmail(username);
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getGrantedAuthorities("USER"));
+	}
+	
+	private List<GrantedAuthority> getGrantedAuthorities(String role) {
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+		return authorities;
+	}
+}
