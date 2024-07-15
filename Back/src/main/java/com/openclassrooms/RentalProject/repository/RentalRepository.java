7f9ca2ee53e1d84a8e6bc026cae504c2d5494440
Andrea Franco <dedefac96@gmail.com>
2024-07-15 10:13:04 +0200

    Use JPA instead of Crud repository

--- Back/src/main/java/com/openclassrooms/RentalProject/repository/RentalRepository.java
+++ Back/src/main/java/com/openclassrooms/RentalProject/repository/RentalRepository.java
@@ -1,12 +1,12 @@
 package com.openclassrooms.RentalProject.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.RentalProject.model.Rental;
 
 @Repository
-public interface RentalRepository extends CrudRepository<Rental, Integer> {
+public interface RentalRepository extends JpaRepository<Rental, Integer> {
 
 }
 
--- Back/src/main/java/com/openclassrooms/RentalProject/repository/UserRepository.java
+++ Back/src/main/java/com/openclassrooms/RentalProject/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.openclassrooms.RentalProject.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.RentalProject.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-
+public interface UserRepository extends JpaRepository<User, Integer> {
+	public User findByEmail(String email);
 }
